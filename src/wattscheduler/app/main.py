a1a069b5ec49e217d25ea2334f57e9cd6549c764
Mikko Mehtonen <mehtonen@gmail.com>
2026-06-13 13:42:07 +0300

    feat(ui): replace favicon with SVG and add dedicated route

--- src/wattscheduler/app/main.py
+++ src/wattscheduler/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from wattscheduler.app.api.routes_health import router as health_router
 from wattscheduler.app.api.routes_schedule import router as schedule_router
 from wattscheduler.app.api.routes_prices import router as prices_router
@@ -15,3 +16,7 @@ app.include_router(ui_router)
 
 # Serve static files
 app.mount("/static", StaticFiles(directory="src/wattscheduler/app/ui/static"), name="static")
+
+@app.get("/favicon.svg", include_in_schema=False)
+async def favicon():
+    return FileResponse("src/wattscheduler/app/ui/static/favicon.svg")
